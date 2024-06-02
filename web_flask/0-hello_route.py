691daa26f0323bc66cd9d771926eb90945b10929
rzsamrio <samuelekop144@gmail.com>
2024-06-02 15:51:12 +0100

    include docs

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """ Return message on open """
     return "<p>Hello HBNB!</p>"
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
