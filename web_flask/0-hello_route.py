a35208bf41bd13be6b3153b35dc903896b01eb99
rzsamrio <samuelekop144@gmail.com>
2024-06-02 15:10:27 +0100

    [WEBFLASK] Add documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" My First Web app """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ Return message on open """
     return "<p>Hello HBNB!</p>"
 
 if __name__ == '__main__':
