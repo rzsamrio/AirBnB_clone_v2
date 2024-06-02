f262d91d34122aa520d9a1773f5683ed161629a0
rzsamrio <samuelekop144@gmail.com>
2024-06-02 16:08:08 +0100

    Add task 1, fix html return on task 0

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,6 +10,11 @@ def hello():
     """ Return message on open """
     return "Hello HBNB!"
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display on route """
+    return "HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
